4cd3d7213419269286997d0691e27ac41cda8f29
sanyam <sanyam9922@gmail.com>
2025-03-23 11:00:21 -0400

    spinned added

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -8,6 +8,7 @@ import { ModeToggle } from '@/components/mode-toggle';
 import { useConvexAuth } from 'convex/react';
 import { Button } from '@/components/ui/button';
 import { SignInButton } from '@clerk/clerk-react';
+import { Spinner } from '@/components/spinner';
 
 const Navbar = () => {
   const scrolled = useScrollTop();
@@ -15,9 +16,9 @@ const Navbar = () => {
   return (
     <div className={cn("z-50 bg-background dark:bg-[#1F1F1F] sticky top-0 flex items-center w-full p-6", scrolled && "border-b shadow-sm")}>
         <Logo />
-        <div className='md:ml-auto md:justify-end justify-between w-full flex items center gap-x-2'>
+        <div className='md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2'>
             {isLoading && (
-              <p>Loading...</p>
+              <Spinner />
             )}
             {!isAuthenticated && !isLoading && (
               <>
