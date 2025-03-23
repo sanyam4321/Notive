adf2110e434df55d0d8392ddccb17ac4c36955e0
sanyam <sanyam9922@gmail.com>
2025-03-23 10:36:58 -0400

    auth initialized

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -9,7 +9,7 @@ const MarketingLayout = ({
   return (
     <div className='h-full dark:bg-[#1F1F1F]'>
         <Navbar />
-        <main className='h-full pt-40'>
+        <main className='h-full pt-15'>
             {children}
         </main>
     </div>
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://grateful-walleye-29.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
